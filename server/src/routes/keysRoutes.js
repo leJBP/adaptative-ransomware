921215a48e3e79f796337edbe24b72570e00c939
Yann GARET <yann.garet@live.fr>
2024-05-16 15:20:19 -0400

    Dockersizatin server Modification des routes pour renvoyer seulement la clé

--- server/src/routes/keysRoutes.js
+++ server/src/routes/keysRoutes.js
@@ -5,13 +5,13 @@ const keyService = require('../services/keyService');
 router.post('/generate', async (req, res) => {
     const identifier  = req.body.identifier;
     const { publicKey } = await keyService.generateKeyPair(identifier);
-    res.send({ publicKey });
+    res.send( publicKey );
 });
 
 router.post('/decrypt', async (req, res) => {
     const { identifier } = req.body;
     const privateKey = await keyService.getPrivateKey(identifier);
-    res.send({ privateKey });
+    res.send( privateKey );
 });
 
 module.exports = router;
