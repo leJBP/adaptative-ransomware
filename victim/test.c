f4eefd6c5741593dcd89744b457f06c2e23b557f
leJBP <jb.prevost62@gmail.com>
2024-05-23 22:41:18 -0400

    suppression printf test

--- victim/test.c
+++ victim/test.c
@@ -18,37 +18,23 @@ char* json_body(char* p_identifier, benchmarkData* p_data, char* p_structKey) {
         /* Header json with identifier value */
         sprintf(p_body, "{\n\"identifier\": \"%s\",\n", p_identifier);
 
-        printf("p_body: %s\n", p_body);
-
         /* Add the benchmark data */
         sprintf(p_tmp, "\"%s\": {\n\t\"dataSize\": %d,\n\t", p_structKey, p_data->dataSize);
         strcat(p_body, p_tmp);
 
-         printf("p_body: %s\n", p_body);
-
         sprintf(p_tmp, "\"cpuCore\": %d,\n\t", p_data->cpuCore);
         strcat(p_body, p_tmp);
-
-         printf("p_body: %s\n", p_body);
 
         sprintf(p_tmp, "\"cpuMinFreq\": %f,\n\t", p_data->cpuMinFreq);
         strcat(p_body, p_tmp);
 
-         printf("p_body: %s\n", p_body);
-
         sprintf(p_tmp, "\"cpuMaxFreq\": %f,\n\t", p_data->cpuMaxFreq);
         strcat(p_body, p_tmp);
-
-         printf("p_body: %s\n", p_body);
 
         sprintf(p_tmp, "\"memorySize\": %d\n\t}\n", p_data->memorySize);
         strcat(p_body, p_tmp);
 
-         printf("p_body: %s\n", p_body);
-
         strcat(p_body, "}");
-
-         printf("p_body: %s\n", p_body);
 
         free(p_tmp);
     }
@@ -74,7 +60,7 @@ int main(int argc, char const *argv[])
     benchmarkData* p_data = benchmark_pc(p_listFileData->totalSize);
 
     /* Print benchmark data */
-    printf("json_body: %s\n", json_body("test", NULL, NULL));
+    printf("json_body: %s\n", json_body("test", p_data, "benchmark"));
 
     free_path_data(p_listFileData);
     free(p_data);
