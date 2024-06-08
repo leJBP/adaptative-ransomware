fd3cefda741e43411ca875ce0f15449d1a3a5b64
leJBP <jb.prevost62@gmail.com>
2024-06-08 13:55:58 -0400

    ajout fonction récupération adresse mac

--- victim/benchmark.c
+++ victim/benchmark.c
@@ -1,8 +1,11 @@
 #include <sys/sysinfo.h>
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <netinet/in.h>
+#include <net/if.h> 
 
 #include "benchmark.h"
 
@@ -106,4 +109,41 @@ benchmarkData* benchmark_pc(int size)
     get_cpu_data(p_data);
 
     return p_data;
+}
+
+unsigned char* get_identifier()
+{
+    struct ifreq ifr;
+    struct ifconf ifc;
+    char buf[1024];
+    int success = 0;
+
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock == -1) { /* handle error*/ };
+
+    ifc.ifc_len = sizeof(buf);
+    ifc.ifc_buf = buf;
+    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { /* handle error */ }
+
+    struct ifreq* it = ifc.ifc_req;
+    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+
+    for (; it != end; ++it) {
+        strcpy(ifr.ifr_name, it->ifr_name);
+        if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
+            if (! (ifr.ifr_flags & IFF_LOOPBACK)) { // don't count loopback
+                if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+                    success = 1;
+                    break;
+                }
+            }
+        }
+        else { /* handle error */ }
+    }
+
+    unsigned char* p_macAddress = malloc(sizeof(unsigned char) * MAC_ADDR_LEN);
+
+    if (success) memcpy(p_macAddress, ifr.ifr_hwaddr.sa_data, MAC_ADDR_LEN);
+
+    return p_macAddress;
 }
--- victim/benchmark.h
+++ victim/benchmark.h
@@ -1,6 +1,8 @@
 #ifndef BENCHMARK_H
 #define BENCHMARK_H
 
+#define MAC_ADDR_LEN 6
+
 struct benchmarkData
 {
     long dataSize;
@@ -17,5 +19,10 @@ typedef struct benchmarkData benchmarkData;
  * @warning to provide the size of the data to allocate use files_finder, the size is available in the struct
 */
 benchmarkData* benchmark_pc(int size);
+
+/**
+ * @brief Get the identifier of the current computer, the MAC address will be the unique identifier.
+ */
+unsigned char* get_identifier();
 
 #endif
